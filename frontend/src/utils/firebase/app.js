244bbef1da7e97d878847d87668cad829c96220c
miiagrish <miia.karmala@tuni.com>
2024-11-12 18:58:16 +0200

    build

--- frontend/src/utils/firebase/app.js
+++ frontend/src/utils/firebase/app.js
@@ -3,13 +3,14 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID
+  apiKey: "AIzaSyAYCTVY_dsuRnSKc6a3Md0z-TqnbdVvuCE",
+  authDomain: "jaettu-kauppalista.firebaseapp.com",
+  projectId: "jaettu-kauppalista",
+  storageBucket: "jaettu-kauppalista.firebasestorage.app",
+  messagingSenderId: "247413672884",
+  appId: "1:247413672884:web:52c479377d8a1b9f1200e1"
 };
+
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
