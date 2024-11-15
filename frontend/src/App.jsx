2f671401d9ed05265b3e3f8f766746cd6141ef87
miiagrish <miia.karmala@tuni.com>
2024-11-15 18:56:11 +0200

    sharing skeleton

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { collection, getDocs } from 'firebase/firestore';
 import { db } from './utils/firebase/app';
+import { getAuth, onAuthStateChanged } from 'firebase/auth'; // Firebase Authentication import
 import ItemLists from './ItemLists';
 import ListView from './ListView';
 import PinPage from './PinPage'; 
@@ -9,17 +10,37 @@ import PinPage from './PinPage';
 const App = () => {
   const [noteItems, setNoteItems] = useState([]);
   const [userPin, setUserPin] = useState(''); 
+  const [isAuthenticated, setIsAuthenticated] = useState(false); 
 
   useEffect(() => {
-    const fetchLists = async () => {
-      const listsCollection = collection(db, 'lists');
-      const listSnapshot = await getDocs(listsCollection);
-      const lists = listSnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
-      setNoteItems(lists);
-    };
-
-    fetchLists();
+    const auth = getAuth();
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setIsAuthenticated(true);
+        setUserPin(user.uid);
+      } else {
+        setIsAuthenticated(false);
+        setUserPin('');
+      }
+    });
   }, []);
+
+  useEffect(() => {
+    if (isAuthenticated) {
+      const fetchLists = async () => {
+        try {
+          const listsCollection = collection(db, 'lists');
+          const listSnapshot = await getDocs(listsCollection);
+          const lists = listSnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+          setNoteItems(lists);
+        } catch (error) {
+          console.error("Error fetching lists:", error);
+        }
+      };
+
+      fetchLists();
+    }
+  }, [isAuthenticated]);
 
   return (
     <Router>
