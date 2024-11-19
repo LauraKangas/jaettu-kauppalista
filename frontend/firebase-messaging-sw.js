a7d45f99c78e55ceb83d1053cdbce1538abf6be0
miiagrish <miia.karmala@tuni.com>
2024-11-19 12:35:10 +0200

    test

---
+++ frontend/firebase-messaging-sw.js
@@ -0,0 +1,25 @@
+import { onBackgroundMessage } from "firebase/messaging/sw";
+import { initializeApp } from "firebase/app";
+import { getMessaging } from "firebase/messaging";
+
+const firebaseConfig = {
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID
+};
+
+initializeApp(firebaseConfig);
+
+onBackgroundMessage(getMessaging(), (payload) => {
+  console.log("Received background message", payload);
+
+  const notificationTitle = payload.notification.title;
+  const notificationOptions = {
+    body: payload.notification.body,
+  };
+
+  self.registration.showNotification(notificationTitle, notificationOptions);
+});
