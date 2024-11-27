c864cd73874266ca56188c8722296b2e5d7acabf
miiagrish <miia.karmala@tuni.com>
2024-11-27 12:48:14 +0200

    add: remember user

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import ItemLists from './ItemLists';
 import ListView from './ListView';
 import PinPage from './PinPage'; 
